a9d8746016187211dc7a7af2c5ecddb6bbe21a65
warflash <nwiesinger67@gmail.com>
2023-03-22 13:48:17 +0100

    try different alias path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   extends: ['github:warflash/nuxt-layers-base'],
   alias: {
-    '@nuxt/kit': require.resolve('@nuxt/kit/import'),
+    '@nuxt/kit': require.resolve('@nuxt/kit'),
   },
 });
