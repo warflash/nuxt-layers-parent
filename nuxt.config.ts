793ddc7c1cb6882728409ecc7c57b9b83368e978
warflash <nwiesinger67@gmail.com>
2023-03-22 13:37:23 +0100

    explicitly add nuxt kit to alias https://github.com/nuxt/nuxt/issues/19426

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['github:warflash/nuxt-layers-base'],
+  alias: {
+    '@nuxt/kit': '@nuxt/kit',
+  },
 });
